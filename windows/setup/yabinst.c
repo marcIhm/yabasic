0d0a6bed85d7c2bab167e9bd1914e5f0a6e35b0d
U-IHM-NOTEBOOK\Olli <marc@ihm.name>
2016-11-20 10:33:05 +0100

    added dialogs for success and failure, switched to more modern gui

--- windows/setup/yabinst.c
+++ windows/setup/yabinst.c
@@ -1,10 +1,10 @@
 /*
 Install-Program for yabasic
-written by Marc Ihm in 1996.
+Copyright by Marc Ihm 1996 - 2016.
 
   Date of last change:
 */
-#define DOLC                     "November 5, 2016"
+#define DOLC                     "November 20, 2016"
 /*
 
 This file is part of yabasic and may be copied under the terms of
@@ -24,7 +24,7 @@ See www.yabasic.de for details.
 #include <commctrl.h>
 #include <ole2.h>
 #include "resource.h"
-
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
 /*------------ defines -------------------*/
 
@@ -38,14 +38,14 @@ See www.yabasic.de for details.
 #define BASIC_DEMOPROG "demo"
 #define BASIC_README "readme.txt"
 #define BASIC_SETUP "setup.exe"
-#define BASIC_LOG "yabasic.log"
+#define BASIC_LOG "yabasic-setuplog.txt"
+#define BASIC_LICENSE "LICENSE.txt"
 
 /* headings for messageboxes */
-#define INSTALL_HEADING " Install yabasic !"
-#define REMOVE_HEADING " Remove yabasic !"
+#define INSTALL_HEADING " Installing yabasic ..."
+#define REMOVE_HEADING " Removing yabasic ..."
 
 /* basic-defaults */
-#define DEFAULTPATH "c:\\yabasic\\"
 #define DEFAULTFONT "swiss13"
 #define DEFAULTBACKGROUND "255,255,255"
 #define DEFAULTFOREGROUND "0,0,0"
@@ -82,6 +82,12 @@ See www.yabasic.de for details.
 /* standard string length */
 #define SSLEN 2000
 
+/* end-dialog return values */
+#define SHOW_LOG 1
+#define RUN_DEMO 2
+#define READ_DOCU 3
+#define JUST_EXIT 4
+
 #if !defined(TRUE)
 #define TRUE (1==1)
 #endif
@@ -117,7 +123,7 @@ char *installpath;       /* path, where to install */
 char *librarypath;       /* path, where libraries should be installed */
 char *temppath;          /* path to store temporary files */
 char logs[10000];        /* string to compose log-message */
-char logpath[1000] = "undefined";      /* string containing pathname of logfile */
+char logfile[1000] = "undefined";      /* string containing pathname of logfile */
 int here;                /* install in current path ? */
 int install;             /* install or remove ? */
 int copied;              /* true if copy in temp-directory */
@@ -138,7 +144,6 @@ int MyRegs(int); /* add or delete entries to or from registry */
 /* dialog callbacks */
 BOOL CALLBACK pathdialog(HWND, UINT, WPARAM, LPARAM);/* choose installpath */
 BOOL CALLBACK progressdialog(HWND, UINT, WPARAM, LPARAM);/* show progress */
-UINT CALLBACK HookProc(HWND, UINT, WPARAM, LPARAM); /* hook for save as */
 
 /* registry manipulation */
 int delreg(HKEY, char *, char *); /* delete keys from Registry */
@@ -162,13 +167,15 @@ char *formatsemver(SEMVER *,char *); /* return string with formatted version num
 /* miscellanous functions */
 void end(int); /* display message and terminate */
 char *app(char *trail); /* append trail to installpath */
-int Copy(char *, char *, int); /* copy files */
+int copy_file(char *, char *, int); /* copy files */
 char *brushup(char *); /* change to upper case, add slash */
 char *enumfiles(int); /* give filenames, one after another */
 LINKINFO *enumlinks(int); /* give back links, one after the other */
 void logit(char *); /* write text to logfile */
 int MyMessage(HWND, LPCSTR, LPCSTR, UINT); /* wrapper for MessageBox() */
 char *reportdir(char *); /* generate a string containing filenames in directory */
+char *last_error(); /* get last error as string */
+void center_dialog(HWND); /* center dialog on screen */
 
 /*------------ main program --------------*/
 
@@ -178,14 +185,16 @@ int WINAPI WinMain(HINSTANCE _this_instance,
 	int win_state)
 {
 	SEMVER oldversion;    /* version already installed */
+	char *oldpath;  /* old installation path */
 	FILE *verfile;       /* file with version */
 	int cancel;          /* return value of dialog */
 	int success;         /* return value of My..() functions */
 	char string[SSLEN];  /* multi-purpose string */
-	char string2[SSLEN]; /* multi-purpose string */
-	char string3[SSLEN]; /* multi-purpose string */
-	char errmsg[SSLEN];  /* for error messages */
+	char string2[SSLEN]; 
+	char string3[SSLEN]; 
+	char errmsg[SSLEN];
 	char *full;
+	int answer;
 	int i;
 
 
@@ -232,7 +241,7 @@ int WINAPI WinMain(HINSTANCE _this_instance,
 		char dirglob[SSLEN];
 
 		/* set advance for progresscount */
-		total_progress = 28;
+		total_progress = 29;
 
 		/* write contents of current directory to logfile */
 		sprintf(dirglob, "%s*.*", currentpath);
@@ -307,14 +316,25 @@ int WINAPI WinMain(HINSTANCE _this_instance,
 		}
 
 		/* get path */
-		installpath = getreg("path");
-		if (!installpath) installpath = DEFAULTPATH;
+		oldpath = brushup(getreg("path"));
+		installpath = malloc(SSLEN + 1);
+		SHGetSpecialFolderPath(0, installpath, CSIDL_PROGRAM_FILES, FALSE);
+		strcat(installpath, "\\yabasic");
 		cancel = !DialogBox((HANDLE)this_instance,
 			MAKEINTRESOURCE(IDD_PATHDIALOG),
 			(HWND)NULL, (DLGPROC)pathdialog);
 		if (cancel) end(INSTALL_CANCELLED);
 		installpath = brushup(installpath);
 		here = !strcmp(currentpath, installpath);
+		if (oldpath && strcmp(installpath, oldpath)) {
+			sprintf(string, "Yabasic will be installed in:\n\n\t%s\n\n"
+				"However, an old installation of yabasic has been found in:\n\n\t%s\n\n"
+				"After this installation has finished, you can safely\n"
+				"remove the old directory by hand !\n\n"
+				"Shall the installation continue ?", installpath, oldpath);
+			answer = MyMessage(NULL, string, INSTALL_HEADING, MB_YESNO | MB_SYSTEMMODAL | MB_ICONINFORMATION);
+			if (answer == IDNO) end(INSTALL_CANCELLED);
+		}
 
 		/* get library path */
 		librarypath = malloc(strlen(installpath) + 10);
@@ -405,7 +425,7 @@ int WINAPI WinMain(HINSTANCE _this_instance,
 		logit(logs);
 
 		/* set advance for progresscount */
-		total_progress = 9;
+		total_progress = 12;
 
 		here = !strcmp(currentpath, installpath);
 
@@ -513,24 +533,15 @@ BOOL CALLBACK pathdialog(HWND handle, UINT message,
 	int cmdid;           /* id of command */
 	char buf[SSLEN];       /* buffer for installation-path */
 	int offset = 0;
-	char name[SSLEN];       /* buffer for filename */
-	OPENFILENAME fname; /* for common dialog */
-	int ret;
+	char name[SSLEN];       /* directory name */
+	BROWSEINFO bi;
+	LPITEMIDLIST pidl;
 
 	switch (message) {
 	case WM_INITDIALOG:
-	{ /* center dialog box */
-		RECT rc;
-
-		GetWindowRect(handle, &rc);
-		SetWindowPos(handle, HWND_TOP,
-			((GetSystemMetrics(SM_CXSCREEN) - (rc.right - rc.left)) / 2),
-			((GetSystemMetrics(SM_CYSCREEN) - (rc.bottom - rc.top)) / 2),
-			0, 0, SWP_NOSIZE | SWP_NOACTIVATE | SWP_SHOWWINDOW);
-	}
-	SetDlgItemText(handle, IDINSTPATH, installpath);
-	return TRUE;
-
+		center_dialog(handle);
+		SetDlgItemText(handle, IDINSTPATH, installpath);
+		return TRUE;
 	case WM_COMMAND:
 		cmdid = LOWORD(wparam);
 		switch (cmdid) {
@@ -545,35 +556,12 @@ BOOL CALLBACK pathdialog(HWND handle, UINT message,
 			EndDialog(handle, FALSE);
 			return TRUE;
 		case IDC_BROWSE:
-			fname.lStructSize = sizeof(fname);
-			fname.hwndOwner = handle;
-			fname.hInstance = this_instance;
-			fname.lpstrFilter = "None\0+.+++\0\0";
-			fname.lpstrCustomFilter = NULL;
-			fname.nMaxCustFilter = 0;
-			fname.nFilterIndex = 0;
-			fname.nMaxFile = SSLEN;
-			fname.lpstrFileTitle = NULL;
-			fname.nMaxFileTitle = 0;
-			offset = 0;
-			GetDlgItemText(handle, IDINSTPATH, buf, SSLEN);
-			ret = GetFileAttributes(buf);
-			if (ret == 0xFFFFFFFF || !(ret & FILE_ATTRIBUTE_DIRECTORY)) {
-				GetLogicalDriveStrings(SSLEN, buf);
-				offset = strlen(buf) + 1;
-			}
-			fname.lpstrInitialDir = buf + offset;
-			strcpy(name, "dummy");
-			fname.lpstrFile = name;
-			fname.lpstrTitle = "Select yabasic's directory";
-			fname.Flags = OFN_HIDEREADONLY | OFN_EXPLORER |
-				OFN_ENABLEHOOK | OFN_ENABLETEMPLATE | OFN_NOVALIDATE;
-			fname.lpstrDefExt = NULL;
-			fname.lCustData = 0;
-			fname.lpfnHook = HookProc;
-			fname.lpTemplateName = MAKEINTRESOURCE(IDD_EXPLANATION);
-			if (GetOpenFileName(&fname)) {
-				name[fname.nFileOffset] = '\0';
+			logit("--Pathdialog: BROWSE\n");
+			memset(&bi, 0, sizeof(bi));
+			bi.lpszTitle = "Please choose the installation directory of yabasic.";
+			bi.ulFlags = BIF_NEWDIALOGSTYLE;
+			pidl=SHBrowseForFolder(&bi);
+			if (pidl!=0 && SHGetPathFromIDList(pidl, name)) {
 				SetDlgItemText(handle, IDINSTPATH, name);
 			}
 			return TRUE;
@@ -586,29 +574,85 @@ BOOL CALLBACK pathdialog(HWND handle, UINT message,
 	return FALSE;
 }
 
-
-UINT CALLBACK HookProc(HWND hdl, UINT msg, WPARAM wparam, LPARAM lparam) /* hook for save as */
+BOOL CALLBACK errordialog(HWND handle, UINT message,
+	WPARAM wparam, LPARAM lparam)
+	/* callback for reporting error in installation */
 {
-	if (msg == WM_INITDIALOG) {
-		RECT rc;
-		/* center dialog box */
-		GetWindowRect(GetParent(hdl), &rc);
-		SetWindowPos(GetParent(hdl), HWND_TOP,
-			((GetSystemMetrics(SM_CXSCREEN) - (rc.right - rc.left)) / 2),
-			((GetSystemMetrics(SM_CYSCREEN) - (rc.bottom - rc.top)) / 2),
-			0, 0, SWP_NOSIZE | SWP_NOACTIVATE | SWP_SHOWWINDOW);
-		CommDlg_OpenSave_HideControl(GetParent(hdl), cmb1);
-		CommDlg_OpenSave_HideControl(GetParent(hdl), edt1);
-		CommDlg_OpenSave_SetControlText(GetParent(hdl), IDOK, "Okay");
-		CommDlg_OpenSave_SetControlText(GetParent(hdl), IDCANCEL, "Cancel");
-		CommDlg_OpenSave_SetControlText(GetParent(hdl), stc4, "Install in: ");
-		CommDlg_OpenSave_HideControl(GetParent(hdl), stc3);
-		CommDlg_OpenSave_HideControl(GetParent(hdl), stc2);
-		return FALSE;
+	int cmdid;           /* id of command */
+
+	switch (message) {
+	case WM_INITDIALOG:
+		center_dialog(handle);
+		SetDlgItemText(handle, IDC_ERRORTEXT, (LPCSTR)lparam);
+		logit("--Errordialog: Initialized\n");
+		return TRUE;
+	case WM_COMMAND:
+		cmdid = LOWORD(wparam);
+		switch (cmdid) {
+		case IDC_BUTTON_VIEW_LOG:
+			logit("--Errordialog: View log\n");
+			EndDialog(handle, SHOW_LOG);
+			return TRUE;
+		case IDC_BUTTON_JUST_EXIT:
+			logit("--Errordialog: Just exit\n");
+			EndDialog(handle, JUST_EXIT);
+			return TRUE;
+		default:
+			break;
+		}
+	default:
+		break;
 	}
 	return FALSE;
 }
 
+BOOL CALLBACK successdialog(HWND handle, UINT message,
+	WPARAM wparam, LPARAM lparam)
+	/* callback for reporting success of installation */
+{
+	int cmdid;           /* id of command */
+	
+	switch (message) {
+	case WM_INITDIALOG:
+		center_dialog(handle);
+		SetDlgItemText(handle, IDC_SUCCESSTEXT, (LPCSTR) lparam);
+		logit("--Successdialog: Initialized\n");
+		return TRUE;
+	case WM_COMMAND:
+		cmdid = LOWORD(wparam);
+		switch (cmdid) {
+		case IDC_BUTTON_RUN_DEMO:
+			logit("--Successdialog: Run demo\n");
+			EndDialog(handle, RUN_DEMO);
+			return TRUE;
+		case IDC_BUTTON_READ_DOCU:
+			logit("--Successdialog: Read docu\n");
+			EndDialog(handle, READ_DOCU);
+			return TRUE;
+		case IDC_BUTTON_JUST_EXIT:
+			logit("--Successdialog: Just exit\n");
+			EndDialog(handle, JUST_EXIT);
+			return TRUE;
+		default:
+			break;
+		}
+	default:
+		break;
+	}
+	return FALSE;
+}
+
+void center_dialog(HWND handle) /* center dialog on screen */
+{
+	RECT rect;
+
+	GetWindowRect(handle, &rect);
+	SetWindowPos(handle, HWND_TOP,
+		((GetSystemMetrics(SM_CXSCREEN) - (rect.right - rect.left)) / 2),
+		((GetSystemMetrics(SM_CYSCREEN) - (rect.bottom - rect.top)) / 2),
+		0, 0, SWP_NOSIZE | SWP_NOACTIVATE | SWP_SHOWWINDOW);
+
+}
 
 int delreg(HKEY start, char *keyname, char *subkey)
 /*
@@ -725,93 +769,117 @@ content : it's content
 }
 
 
-void end(int m) { /* display message and terminate */
+void end(int result) { /* display message and terminate */
 	char *msg;
+	int success = FALSE;
+	int answer;
 	int ret;
-	int ret2;
 	char string[SSLEN];          /* multi-purpose string */
 	char string2[SSLEN];
-	int run_demo = FALSE;
-	int show_log = TRUE;
+	char *se_what = NULL;  /* target for shell execute */
+	char se_desc[SSLEN] = "Just exit";  /* description for shell execute */
+	int se_ret = 33; /* return value okay for shell-execute*/
+	int ask_run_something = FALSE;
+	int ask_show_log = TRUE;
 	char *heading = INSTALL_HEADING;
 
-	switch (m) {
+	switch (result) {
 	case INSTALL_CANCELLED:
 		msg = "Installation cancelled.\n\n"
 			"No garbage has been left.";
-		ret = FALSE;
-		show_log = FALSE;
+		ask_show_log = FALSE;
 		break;
 	case INSTALL_IMPOSSIBLE:
-		msg = "Installation failed !\n\n"
+		msg = "\nInstallation failed !\n\n"
 			"No garbage has been left.";
-		ret = FALSE;
 		break;
 	case INSTALL_FAILURE:
-		msg = "Installation failed !\n"
+		msg = "\nInstallation failed !\n"
 			"Some garbage has been left in the system.\n\n"
 			"To clear up, you better remove "BASIC_NAME" through its entry in control-panel, section software.";
-		ret = FALSE;
 		break;
 	case INSTALL_ABORTED:
-		msg = "Installation aborted.\n"
+		msg = "\nInstallation aborted.\n"
 			"Some garbage has been left in the system !\n\n"
 			"To clean up, you better remove "BASIC_NAME" through its entry in control-panel, section software.";
-		ret = FALSE;
 		break;
 	case INSTALL_SUCCESS:
-		msg = "Installation completed successfully !\n\n"
+		msg = "\nInstallation completed successfully !\n\n"
 			"You may now start writing Yabasic-programs.";
-		ret = TRUE;
-		run_demo = TRUE;
-		show_log = FALSE;
+		success = TRUE;
+		ask_run_something = TRUE;
+		ask_show_log = FALSE;
 		break;
 	case REMOVE_SUCCESS:
 		msg = BASIC_NAME" has been removed properly !";
-		ret = TRUE;
-		show_log = FALSE;
+		success = TRUE;
+		ask_show_log = FALSE;
 		heading = REMOVE_HEADING;
 		break;
 	case REMOVE_CANCELLED:
 		msg = "Cancelled. "BASIC_NAME" has been left intact.";
-		ret = FALSE;
-		show_log = FALSE;
+		ask_show_log = FALSE;
 		heading = REMOVE_HEADING;
 		break;
 	case REMOVE_FAILURE:
-		msg = "Couldn't remove "BASIC_NAME" properly !";
-		ret = FALSE;
+		msg = "\nCouldn't remove "BASIC_NAME" properly !";
 		heading = REMOVE_HEADING;
 		break;
 	case SILENT:
-		ret = TRUE;
-		show_log = FALSE;
-		goto silent;
+		success = TRUE;
+		ask_show_log = FALSE;
 	default:
 		break;
 	}
 
 	string2[0] = '\0';
-	if (show_log) sprintf(string2, "\n\n\nDo you want to view the logfile ?\n(%s)\n\nYou may send it to mail@yabasic.de and I will try to help.", logpath);
-	if (run_demo) sprintf(string2, "\n\n\nDo you want to run the demo of yabasic ?");
-	sprintf(string, "\n\n\n%s%s", msg, string2);
-	if (MyMessage(NULL, string, heading, (show_log || run_demo) ? (MB_YESNO | MB_ICONQUESTION | MB_SYSTEMMODAL) : MB_STYLE) == IDYES) {
-		if (show_log) sprintf(string, "%s", logpath);
-		if (run_demo) sprintf(string, "%s/demo.yab", installpath);
-		ret2 = ShellExecute(NULL, "open", string, NULL, NULL, SW_SHOWNORMAL);
-		if (ret2 <= 32) {
-			FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM, NULL, GetLastError(),
-				MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), string, SSLEN, NULL);
-			if (show_log) {
-				sprintf(string2, "Could show setup log (%s): %d, %s", logpath, ret2, string);
-				heading = " Could not show Logfile ! ";
-			}
-			if (run_demo) {
-				sprintf(string2, "Could start demo of yabasic: %d, %s", ret2, string);
-				heading = " Could not show Logfile ! ";
-			}
-			MessageBoxEx(NULL, string2, heading,
-				MB_OK | MB_SYSTEMMODAL | MB_ICONINFORMATION, MAKELANGID(LANG_ENGLISH, SUBLANG_ENGLISH_US));
+	if (ask_run_something) {
+		sprintf(string, "%s\n\n\nDo you want to run the demo of yabasic or read its documentation ?", msg);
+		answer = DialogBoxParam((HANDLE)this_instance,
+			MAKEINTRESOURCE(IDD_ON_SUCCESS),
+			(HWND)NULL, (DLGPROC)successdialog, (LPARAM) string);
+		switch (answer) {
+		case RUN_DEMO:
+			sprintf(string2, "%s/demo.yab", installpath);
+			se_what = string2;
+			sprintf(se_desc, "Start the demo of yabasic: %s/%s", installpath, se_what);
+			break;
+		case READ_DOCU:
+			sprintf(string2, "%s/yabasic.htm", installpath);
+			se_what = string2;
+			sprintf(se_desc,"Show the documentation of yabasic: %s/%s", installpath, se_what);
+			break;
+		}
+	}
+
+	if (ask_show_log) {
+		sprintf(string, "%s\n\n\nDo you want to view the logfile ?\n\n\t%s\n\nYou may send it to mail@yabasic.de and I will try to help.\n", msg, logfile);
+		answer = DialogBoxParam((HANDLE)this_instance,
+			MAKEINTRESOURCE(IDD_ON_ERROR),
+			(HWND)NULL, (DLGPROC)errordialog, (LPARAM) string);
+		switch (answer) {
+		case SHOW_LOG:
+			se_what = logfile;
+			sprintf(se_desc, "Show the setup-log of yabasic: %s", logfile);
+			break;
+		}
+	}
+
+	if (!ask_run_something && !ask_show_log && result!=SILENT) {
+		sprintf(string, "%s", msg);
+		MyMessage(NULL, string, heading, MB_STYLE);
+	}
+
+	if (se_what) {
+		se_ret = ShellExecute(NULL, "open", se_what, NULL, NULL, SW_SHOWNORMAL);
+		if (se_ret <= 32) {
+			sprintf(string, "Could not '%s': %s", se_desc, last_error());
+			MyMessage(NULL, string, "Failed to execute !", MB_OK | MB_SYSTEMMODAL | MB_ICONINFORMATION);
+			success = FALSE;
+		}
+		else {
+			sprintf(string, "Successfully executed: %s", se_desc);
+			logit(string);
 		}
 	}
 
@@ -820,7 +888,7 @@ silent:
 	/* close log-file */
 	logit(NULL);
 
-	exit(ret);
+	exit(success ? 0 : 1);
 
 	return;
 }
@@ -962,11 +1030,13 @@ int MyLinks(int mode) /* add or remove shell links */
 		if (mode == INSTALL && !li->removeonly) {
 			sprintf(string, "Adding %s", li->desc);
 			progress(string);
+			logit(string);
 			success = SUCCEEDED(CreateShellLink(li, installpath)) && success;
 		}
 		else {
 			sprintf(string, "removing %s", li->desc);
 			progress(string);
+			logit(string);
 			success = SUCCEEDED(DeleteShellLink(li)) && success;
 		}
 	}
@@ -995,7 +1065,7 @@ int MyFiles(int mode) /* copy or delete files */
 
 		while (success && (name = enumfiles(NEXT))) {
 			sprintf(string, "%s%s", currentpath, name);
-			success = Copy(string, app(name), here) && success;
+			success = copy_file(string, app(name), here) && success;
 			sprintf(string, "Copying '%s'", name);
 			progress(string);
 		}
@@ -1095,16 +1165,8 @@ BOOL CALLBACK progressdialog(HWND handle, UINT message,
 
 	switch (message) {
 	case WM_INITDIALOG:
-	{ /* center dialog box */
-		RECT rc;
-
-		GetWindowRect(handle, &rc);
-		SetWindowPos(handle, HWND_TOP,
-			((GetSystemMetrics(SM_CXSCREEN) - (rc.right - rc.left)) / 2),
-			((GetSystemMetrics(SM_CYSCREEN) - (rc.bottom - rc.top)) / 2),
-			0, 0, SWP_NOSIZE | SWP_NOACTIVATE | SWP_SHOWWINDOW);
-	}
-	return TRUE;
+		center_dialog(handle);
+		return TRUE;
 	default:
 		break;
 	}
@@ -1167,7 +1229,7 @@ void progress(char *msg) /* show progress */
 }
 
 
-int Copy(char *name, char *dest, int here) /* copy files */
+int copy_file(char *name, char *dest, int here) /* copy files */
 {
 	int ret;
 	char string[SSLEN];
@@ -1185,6 +1247,13 @@ int Copy(char *name, char *dest, int here) /* copy files */
 	return ret;
 }
 
+char *last_error() /* get last error as string */
+{
+	static char text[SSLEN];
+	FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM, NULL, GetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), text, SSLEN, NULL);
+	return text;
+}
 
 char *enumfiles(int mode) /* give filenames, one after another */
 {
@@ -1203,6 +1272,7 @@ char *enumfiles(int mode) /* give filenames, one after another */
 	if (num == 4) return strdup(BASIC_DEMOPROG".yab");
 	if (num == 5) return strdup(BASIC_README);
 	if (num == 6) return strdup(BASIC_NAME".htm");
+	if (num == 7) return strdup(BASIC_LICENSE);
 
 	return NULL;
 }
@@ -1275,10 +1345,10 @@ void logit(char *text)
 
 	/* open log-file */
 	if (!log) {
-		sprintf(logpath, "%s%s", temppath, BASIC_LOG);
-		log = fopen(logpath, "a");
+		sprintf(logfile, "%s%s", temppath, BASIC_LOG);
+		log = fopen(logfile, "a");
 		if (!log) {
-			sprintf(string, "Could not open log file '%s': %s", logpath, strerror(errno));
+			sprintf(string, "Could not open log file '%s': %s", logfile, strerror(errno));
 			MyMessage(NULL, string, INSTALL_HEADING, MB_STYLE);
 			end(INSTALL_IMPOSSIBLE);
 		}
