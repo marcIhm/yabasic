4c0b4ff623de6aa9db77fe9d3a7fdbfe2db0bd30
Marc Ihm <marc@ihm.name>
2019-01-02 15:46:40 +0100

    advanced version

--- unix/lang/config.h
+++ unix/lang/config.h
@@ -2,7 +2,7 @@
 /* config.h.in.  Generated from configure.ac by autoheader.  */
 
 /* build-time, that will be displayed in banner */
-#define BUILD_TIME "Tue Jan  1 19:56:47 UTC 2019"
+#define BUILD_TIME "Wed Jan  2 14:45:04 UTC 2019"
 
 /* Define to one of `_getb67', `GETB67', `getb67' for Cray-2 and Cray-YMP
    systems. This function is required for `alloca.c' support on those systems.
@@ -199,7 +199,7 @@
 #define PACKAGE_NAME "yabasic"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "yabasic 2.81.1"
+#define PACKAGE_STRING "yabasic 2.81.2"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "yabasic"
@@ -208,7 +208,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "2.81.1"
+#define PACKAGE_VERSION "2.81.2"
 
 /* Define as the return type of signal handlers (`int' or `void'). */
 #define RETSIGTYPE void
@@ -243,7 +243,7 @@
 #define UNIX_ARCHITECTURE "x86_64-unknown-linux-gnu"
 
 /* Version number of package */
-#define VERSION "2.81.1"
+#define VERSION "2.81.2"
 
 /* Define to 1 if the X Window System is missing or not being used. */
 /* #undef X_DISPLAY_MISSING */
