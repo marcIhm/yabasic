bdd9cbe17b3535e590da785030e5faab58752943
Jonathan Lovelace <kingjon3377@gmail.com>
2019-02-08 23:26:52 -0500

    Quote $OUT in silent.sh.
    If it's empty or just whitespace, Bash will print the error
    'Unary operator expected'; if it contains more than one word, or a
    wildcard, Bash would split it into multiple words (breaking the [ ... ]
    test that way as well) and try to expand the wildcards.

--- unix/lang/tests/silent.sh
+++ unix/lang/tests/silent.sh
@@ -4,8 +4,8 @@
 # Check, if empty yabasic-program produces any warning
 #
 OUT=`./yabasic tests/resources/silent.yab 2>&1`
-echo $OUT
-if [ $OUT -ne "" ]; then
+echo "$OUT"
+if [ "$OUT" -ne "" ]; then
     exit 1
 else
     exit 0
