5084d5b72e8a6af287ad9ada6950802b0e6481f7
Marc Ihm <marc@ihm.name>
2019-03-20 06:43:04 +0100

    added frame for ffi code

---
+++ unix/lang/external.c
@@ -0,0 +1,36 @@
+/*
+
+    YABASIC ---  a simple Basic Interpreter
+    written by Marc Ihm 1995-2019
+    more info at www.yabasic.de
+
+    external.c --- code for calling external libraries
+
+    This file is part of yabasic and may be copied under the terms of
+    MIT License which can be found in the file LICENSE.
+
+*/
+
+/* ------------- includes ---------------- */
+
+#ifndef YABASIC_INCLUDED
+#include "yabasic.h"		/* all prototypes and structures */
+#endif
+
+
+/* ------------- external references ---------------- */
+
+
+/* ------------- local functions ---------------- */
+
+
+/* ------------- global variables ---------------- */
+
+
+/* ------------- subroutines ---------------- */
+
+#ifdef WINDOWS
+#ifdef HAVE_FFI
+#endif
+#else
+#endif
