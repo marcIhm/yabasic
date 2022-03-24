4dd39b0cab9dfac6049e3722b310be4e9adcc81e
Marc Ihm <marc@ihm.name>
2022-03-24 21:10:37 +0100

    ship some more

--- unix/lang/config.h
+++ unix/lang/config.h
@@ -2,7 +2,7 @@
 /* config.h.in.  Generated from configure.ac by autoheader.  */
 
 /* build-time, that will be displayed in banner */
-#define BUILD_TIME "Thu Mar 24 17:55:05 UTC 2022"
+#define BUILD_TIME "Thu Mar 24 20:07:28 UTC 2022"
 
 /* Define to one of `_getb67', `GETB67', `getb67' for Cray-2 and Cray-YMP
    systems. This function is required for `alloca.c' support on those systems.
